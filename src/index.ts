01521254bc5457853aff9cb22c5b0391804d563b
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2024-02-16 14:53:49 +0530

    feat: implement TCP echo server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-// @ts-ignore
-import { Elm } from "../dist/elm.js";
+import net from "net";
 
-const app = Elm.Main.init();
+const PORT = 5379;
+const HOST = "0.0.0.0";
 
-app.ports.log && app.ports.log.subscribe(console.log);
+// Create a TCP server
+const server = net.createServer((socket) => {
+  console.log("Client connected:", socket.remoteAddress, socket.remotePort);
+
+  // Echo incoming data
+  socket.on("data", (data) => {
+    socket.write(data.toString());
+  });
+
+  // Handle client disconnection
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+// Start listening
+server.listen(PORT, HOST, () => {
+  console.log(`TCP server listening on ${HOST}:${PORT}`);
+});
