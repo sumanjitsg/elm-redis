84d0592560624b46675fb7920b7c6e0b6ec2cfef
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2024-02-20 09:42:52 +0530

    test(resp): add error test cases for simple string parser
    TODO:
    Fix failing error tests because Parser.deadEndsToString always returns "TODO deadEndsToString"

--- tests/RespTest.elm
+++ tests/RespTest.elm
@@ -8,21 +8,28 @@ import Test exposing (..)
 
 testSuite : Test
 testSuite =
-    describe "RESP simpleStringParser"
+    describe "simpleStringParser"
         [ test "parses simple strings correctly" <|
             \_ ->
                 "+OK\u{000D}\n"
                     |> Parser.run Resp.simpleStringParser
                     |> Expect.equal (Ok "OK")
-
-        -- , test "fails on strings without leading '+'" <|
-        --     \_ ->
-        --         "OK\u{000D}\n"
-        --             |> Parser.run Resp.simpleString
-        --             |> Expect.equal (Err "Expected '+' at position 1 but saw 'O'")
-        -- , test "fails on strings without trailing '\\r\\n'" <|
-        --     \_ ->
-        --         "+OK\n"
-        --             |> Parser.run Resp.simpleString
-        --             |> Expect.equal (Err "Expected '\\r\\n' at position 4 but saw '\\n'")
+        , test "fails on strings without leading '+'" <|
+            \_ ->
+                let
+                    result =
+                        Parser.run Resp.simpleStringParser "OK\u{000D}\n"
+                            |> Result.mapError Parser.deadEndsToString
+                in
+                result
+                    |> Expect.equal (Err "Expected '+' at position 1 but saw 'O'")
+        , test "fails on strings without trailing '\\r\\n'" <|
+            \_ ->
+                let
+                    result =
+                        Parser.run Resp.simpleStringParser "+OK\n"
+                            |> Result.mapError Parser.deadEndsToString
+                in
+                result
+                    |> Expect.equal (Err "Expected '\\r\\n' at position 4 but saw '\\n'")
         ]
