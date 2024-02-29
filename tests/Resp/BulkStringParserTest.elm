9a6bc8f7d525e37c0277f1c3ef86134361fc9e26
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2024-02-29 18:50:19 +0530

    test(resp): add negative test cases for bulk string parser

--- tests/Resp/BulkStringParserTest.elm
+++ tests/Resp/BulkStringParserTest.elm
@@ -24,4 +24,62 @@ testSuite =
                 "$-1\u{000D}\n"
                     |> Parser.run Resp.BulkStringParser.parser
                     |> Expect.equal (Ok Nothing)
+        , Test.test "fails on bulk strings without leading '$'" <|
+            \_ ->
+                "5\u{000D}\nhello\u{000D}\n"
+                    |> Parser.run Resp.BulkStringParser.parser
+                    |> Result.mapError (List.map .problem)
+                    |> Expect.equal (Err [ Resp.BulkStringParser.problemExpectingDollar ])
+        , Test.test "fails on bulk strings without the integer" <|
+            \_ ->
+                "$\u{000D}\nhello\u{000D}\n"
+                    |> Parser.run Resp.BulkStringParser.parser
+                    |> Result.mapError (List.map .problem)
+                    |> Expect.equal
+                        (Err
+                            [ Resp.BulkStringParser.problemExpectingMinusOne
+                            , Resp.BulkStringParser.problemExpectingInteger
+                            ]
+                        )
+        , Test.test "fails on bulk strings with a string in place of the integer" <|
+            \_ ->
+                "$hello\u{000D}\nhello\u{000D}\n"
+                    |> Parser.run Resp.BulkStringParser.parser
+                    |> Result.mapError (List.map .problem)
+                    |> Expect.equal
+                        (Err
+                            [ Resp.BulkStringParser.problemExpectingMinusOne
+                            , Resp.BulkStringParser.problemExpectingInteger
+                            ]
+                        )
+        , Test.skip <|
+            Test.test "fails on bulk strings with an integer less than -1" <|
+                \_ ->
+                    "$-13\u{000D}\nhello\u{000D}\n"
+                        |> Parser.run Resp.BulkStringParser.parser
+                        |> Result.mapError (List.map .problem)
+                        |> Expect.equal
+                            (Err
+                                [ Resp.BulkStringParser.problemExpectingMinusOne
+                                , Resp.BulkStringParser.problemExpectingInteger
+                                ]
+                            )
+        , Test.test "fails on bulk strings with length mismatch" <|
+            \_ ->
+                "$5\u{000D}\nhell\u{000D}\n"
+                    |> Parser.run Resp.BulkStringParser.parser
+                    |> Result.mapError (List.map .problem)
+                    |> Expect.equal (Err [ Resp.BulkStringParser.problemExpectingLengthMismatch ])
+        , Test.test "fails on bulk strings without trailing '\\r\\n'" <|
+            \_ ->
+                "$5\u{000D}\nhello"
+                    |> Parser.run Resp.BulkStringParser.parser
+                    |> Result.mapError (List.map .problem)
+                    |> Expect.equal (Err [ Resp.BulkStringParser.problemExpectingCrlf ])
+        , Test.test "fails on bulk strings without '\\r\\n' between the integer and the data" <|
+            \_ ->
+                "$5hello\u{000D}\n"
+                    |> Parser.run Resp.BulkStringParser.parser
+                    |> Result.mapError (List.map .problem)
+                    |> Expect.equal (Err [ Resp.BulkStringParser.problemExpectingCrlf ])
         ]
