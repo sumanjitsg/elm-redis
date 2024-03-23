01bffa086a0da173199a71a32bac555af1357f03
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2024-03-23 19:08:41 +0530

    feat(resp): parse array of bulk strings

--- src/Resp/ArrayParser.elm
+++ src/Resp/ArrayParser.elm
@@ -1,6 +1,7 @@
 module Resp.ArrayParser exposing (parser)
 
 import Parser.Advanced as Parser exposing ((|.), (|=))
+import Resp.BulkStringParser
 import Resp.Problem
 import Resp.SimpleStringParser
 
@@ -10,23 +11,30 @@ import Resp.SimpleStringParser
 -- PARSER
 
 
-parser : Parser.Parser () Resp.Problem.Problem (List String)
+parser : Parser.Parser () Resp.Problem.Problem (List (Maybe String))
 parser =
     Parser.succeed identity
         |. Parser.symbol (Parser.Token "*" Resp.Problem.ExpectingAsterisk)
         |= Parser.int Resp.Problem.ExpectingInteger Resp.Problem.InvalidNumber
         |. Parser.symbol (Parser.Token "\u{000D}\n" Resp.Problem.ExpectingCrlf)
-        |> Parser.andThen (\length -> Parser.loop ( length, [] ) parseList)
+        |> Parser.andThen (\count -> Parser.loop ( count, [] ) parseList)
 
 
-parseList : ( Int, List String ) -> Parser.Parser () Resp.Problem.Problem (Parser.Step ( Int, List String ) (List String))
+parseList : ( Int, List (Maybe String) ) -> Parser.Parser () Resp.Problem.Problem (Parser.Step ( Int, List (Maybe String) ) (List (Maybe String)))
 parseList ( count, list ) =
     if count == 0 then
         Parser.succeed (Parser.Done list)
 
     else
-        Resp.SimpleStringParser.parser
-            |> Parser.andThen
-                (\value ->
-                    Parser.succeed (Parser.Loop ( count - 1, list ++ [ value ] ))
-                )
+        Parser.oneOf
+            [ Resp.SimpleStringParser.parser
+                |> Parser.andThen
+                    (\value ->
+                        Parser.succeed (Parser.Loop ( count - 1, list ++ [ Just value ] ))
+                    )
+            , Resp.BulkStringParser.parser
+                |> Parser.andThen
+                    (\value ->
+                        Parser.succeed (Parser.Loop ( count - 1, list ++ [ value ] ))
+                    )
+            ]
--- tests/Resp/ArrayParserTest.elm
+++ tests/Resp/ArrayParserTest.elm
@@ -10,14 +10,19 @@ import Test
 testSuite : Test.Test
 testSuite =
     Test.describe "RESP Array Parser"
-        [ Test.test "parses array of simple strings correctly" <|
-            \_ ->
-                "*2\u{000D}\n+hello\u{000D}\n+world\u{000D}\n"
-                    |> Parser.run Resp.ArrayParser.parser
-                    |> Expect.equal (Ok [ "hello", "world" ])
-        , Test.test "parses an empty array correctly" <|
+        [ Test.test "parses an empty array correctly" <|
             \_ ->
                 "*0\u{000D}\n"
                     |> Parser.run Resp.ArrayParser.parser
                     |> Expect.equal (Ok [])
+        , Test.test "parses array of simple strings correctly" <|
+            \_ ->
+                "*2\u{000D}\n+hello\u{000D}\n+world\u{000D}\n"
+                    |> Parser.run Resp.ArrayParser.parser
+                    |> Expect.equal (Ok [ Just "hello", Just "world" ])
+        , Test.test "parses array of bulk strings correctly" <|
+            \_ ->
+                "*2\u{000D}\n$5\u{000D}\nhello\u{000D}\n$5\u{000D}\nworld\u{000D}\n"
+                    |> Parser.run Resp.ArrayParser.parser
+                    |> Expect.equal (Ok [ Just "hello", Just "world" ])
         ]
