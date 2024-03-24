45c3ccbd98cde3cd99916a63a8fc1beb9ed8b302
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2024-03-24 14:14:36 +0530

    test(resp): remove parser import and replaced parser with decoder in simple string test descriptions

--- tests/Resp/SimpleStringTest.elm
+++ tests/Resp/SimpleStringTest.elm
@@ -1,27 +1,26 @@
 module Resp.SimpleStringTest exposing (testSuite)
 
 import Expect
-import Parser.Advanced as Parser
 import Resp
 import Test
 
 
 testSuite : Test.Test
 testSuite =
-    Test.describe "RESP Simple String Parser"
-        [ Test.test "parses non-empty simple strings correctly" <|
+    Test.describe "RESP Simple String Decoder"
+        [ Test.test "decodes non-empty simple strings correctly" <|
             \_ ->
                 "+OK\u{000D}\n"
                     |> Resp.decode Resp.SimpleStringDecoder
                     |> Result.map Resp.toString
                     |> Expect.equal (Ok "OK")
-        , Test.test "parses empty simple strings correctly" <|
+        , Test.test "decodes empty simple strings correctly" <|
             \_ ->
                 "+\u{000D}\n"
                     |> Resp.decode Resp.SimpleStringDecoder
                     |> Result.map Resp.toString
                     |> Expect.equal (Ok "")
-        , Test.test "parses strings containing '+' correctly" <|
+        , Test.test "decodes strings containing '+' correctly" <|
             \_ ->
                 "++O+K+\u{000D}\n"
                     |> Resp.decode Resp.SimpleStringDecoder
