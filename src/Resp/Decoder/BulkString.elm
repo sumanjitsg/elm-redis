b8326adc0ac2de77114cf8d00e8f7dcaf6fbdffc
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2024-03-25 15:08:56 +0530

    feat(resp): remove remnant of older bulk string decoder

--- src/Resp/Decoder/BulkString.elm
+++
@@ -1,9 +0,0 @@
-module Resp.Decoder.BulkString exposing (parser)
-
-import Parser.Advanced as Parser exposing ((|.), (|=))
-import Resp.Decoder
-
-
-
--- TODO: the number is an unsigned, base-10 value (except -1).
--- PARSER
