cfbbbc7f8a20abfed76c463e387dcfd26545a657
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2024-03-26 18:42:04 +0530

    test(resp): replace checking decoder Problem types with only Expect.err in bulk string tests

--- tests/Test/Resp/BulkString.elm
+++ tests/Test/Resp/BulkString.elm
@@ -36,54 +36,45 @@ testSuite =
             \_ ->
                 "5\u{000D}\nhello\u{000D}\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingDollar ])
+                    |> Expect.err
         , Test.test "fails on bulk strings without the integer" <|
             \_ ->
                 "$\u{000D}\nhello\u{000D}\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingValidLength ])
+                    |> Expect.err
         , Test.test "fails on bulk strings with a string in place of the integer" <|
             \_ ->
                 "$hello\u{000D}\nhello\u{000D}\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingValidLength ])
+                    |> Expect.err
         , Test.test "fails on bulk strings with an integer less than -1" <|
             \_ ->
                 "$-13\u{000D}\nhello\u{000D}\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingValidLength ])
+                    |> Expect.err
         , Test.test "fails on bulk strings with length mismatch" <|
             \_ ->
                 "$5\u{000D}\nhell\u{000D}\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingDataOfLength ])
+                    |> Expect.err
         , Test.test "fails on bulk strings without trailing '\\r\\n'" <|
             \_ ->
                 "$5\u{000D}\nhello"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingCrlf ])
+                    |> Expect.err
         , Test.test "fails on bulk strings without '\\r\\n' between the integer and the data" <|
             \_ ->
                 "$5hello\u{000D}\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingValidLength ])
+                    |> Expect.err
         , Test.test "fails on error strings having '\\n' instead of '\\r\\n' after the integer" <|
             \_ ->
                 "$2\nOK\u{000D}\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingValidLength ])
+                    |> Expect.err
         , Test.test "fails on error strings ending with '\\n' instead of '\\r\\n'" <|
             \_ ->
                 "$2\u{000D}\nOK\n"
                     |> Resp.decode Resp.BulkStringDecoder
-                    |> Result.mapError (List.map .problem)
-                    |> Expect.equal (Err [ Resp.ExpectingCrlf ])
+                    |> Expect.err
         ]
